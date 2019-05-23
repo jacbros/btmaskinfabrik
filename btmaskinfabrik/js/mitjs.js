e6483f6a40d7a02ada30744abef84b170cb80016
jacbros <44459980+jacbros@users.noreply.github.com>
2019-05-23 09:42:36 +0200

    Add files via upload

---
+++ btmaskinfabrik/js/mitjs.js
@@ -0,0 +1,22 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var sub = this.nextElementSibling;
+        if (sub.style.display === "block") {
+            sub.style.display = "none";
+        } else {
+            sub.style.display = "block";
+        }
+    });
+}
+
+function menuToggle() {
+    var x = document.getElementById("menu");
+    x.classList.toggle("menuactive");
+}
+
+
+//Hovedprogramdel
